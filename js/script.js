3800ade54e702e53ba0d4aa8f404e5f2be089786
shakhawat <shakhawathossen188@gmail.com>
2024-09-19 08:48:04 +0600

    home find property and why choose us section desigh

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.slider_section').slick({
+    dots: true,       // Show dots (pagination)
+    infinite: true,   // Infinite loop
+    speed: 500,       // Transition speed in milliseconds
+    slidesToShow: 3,  // Number of slides to show
+    slidesToScroll: 1, // Number of slides to scroll at a time
+    autoplay: true,   // Enable auto-play
+    autoplaySpeed: 3000, // Auto-play speed
+  });
